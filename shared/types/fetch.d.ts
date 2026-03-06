86511d1cc640424436795c0c4bbe3fe6038350b0
ram-a-dhan <ram.adhan@outlook.com>
2026-03-06 17:40:08 +0700

    fix: add missing pagination response to GET users

--- shared/types/fetch.d.ts
+++ shared/types/fetch.d.ts
@@ -1,13 +1,31 @@
 export interface IParams<T = {}> {
   page?: number;
-  limit?: number;
+  size?: number;
   sort?: keyof T;
   order?: "asc" | "desc";
   search?: string;
 }
 
-export interface IStatus<T = any> {
+export type IRes<T = any> = T extends void ? {
   statusCode: number;
   statusMessage: string;
-  data?: T;
+} : {
+  statusCode: number;
+  statusMessage: string;
+  data: T;
+}
+
+export type IResPage<T = any> = T extends void ? {
+  statusCode: number;
+  statusMessage: string;
+} : {
+  statusCode: number;
+  statusMessage: string;
+  data: T;
+  paging: IPaging;
+}
+
+export interface IPaging {
+  page: number;
+  size: number;
 }
