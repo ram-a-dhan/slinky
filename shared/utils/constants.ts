76b7a2232682de2bccc1bb332a0ea229276be169
ram-a-dhan <ram.adhan@outlook.com>
2026-03-07 12:31:16 +0700

    feat: separate time logging from GET and PATCH user by id into dedicated endpoint

--- shared/utils/constants.ts
+++ shared/utils/constants.ts
@@ -1,17 +1,18 @@
 export const HTTP_STATUS = {
   OK: 200,
   CREATED: 201,
+  NO_CONTENT: 204,
   BAD_REQUEST: 400,
   NOT_FOUND: 404,
   INTERNAL_SERVER_ERROR: 500,
-};
+} as const;
 
 export const HTTP_METHOD = {
   GET: "GET",
   POST: "POST",
   PATCH: "PATCH",
   DELETE: "DELETE",
-};
+} as const;
 
 export const REGEX = {
   EMAIL: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
