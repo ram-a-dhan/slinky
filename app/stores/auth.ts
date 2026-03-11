f6b4a43293d07222960b5bc76059a8d0a437639b
ram-a-dhan <ram.adhan@outlook.com>
2026-03-11 13:37:00 +0700

    feat: add google authentication

---
+++ app/stores/auth.ts
@@ -0,0 +1,45 @@
+import { defineStore } from "pinia";
+
+export const useAuthStore = defineStore("auth", () => {
+  const user = ref<IUser | null>(null);
+  const isLoading = ref<boolean>(false);
+  const isLoggedIn = computed(() => user.value !== null);
+
+  const fetchUser = async () => {
+    try {
+      isLoading.value = true;
+
+      const headers = useRequestHeaders(["cookie"]);
+      user.value = await $fetch("/api/auth/session", { headers });
+    } catch {
+      user.value = null;
+    } finally {
+      isLoading.value = false;
+    }
+  };
+
+  const signIn = async () => {
+    window.location.href = "/api/auth/google";
+  };
+
+  const signOut = async () => {
+    const logout = await $fetch<{ success: boolean }>(
+      "/api/auth/logout",
+      { method: HTTP_METHOD.POST },
+    );
+
+    if (logout.success) {
+      user.value = null;
+      await navigateTo("/");
+    }
+  };
+
+  return {
+    user,
+    isLoading,
+    isLoggedIn,
+    fetchUser,
+    signIn,
+    signOut,
+  };
+});
