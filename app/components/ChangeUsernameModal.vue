2180ed304f943b54d24d7462c10e3ca5b9f8c8b2
ram-a-dhan <ram.adhan@outlook.com>
2026-03-16 08:00:55 +0700

    feat: add change username feature

---
+++ app/components/ChangeUsernameModal.vue
@@ -0,0 +1,161 @@
+<script setup lang="ts">
+import type { FormSubmitEvent } from "@primevue/forms";
+
+interface IFormValues {
+  username: string;
+}
+
+const visible = defineModel<boolean>("visible", { required: true });
+
+const loading = ref(false);
+const initialValues = ref<IFormValues>({ username: "" });
+const formKey = ref(0);
+
+const auth = useAuthStore();
+const toast = useToast();
+
+watch(visible, (val) => {
+  if (val === true) {
+    initialValues.value.username = auth.user?.username || "";
+    formKey.value++;
+  }
+});
+
+const resolver: IFormResolver<IFormValues> = ({ values }) => {
+  const errors: IFormResolverReturn<IFormValues>["errors"] = { username: [] };
+
+  if (!values.username) {
+    errors.username.push({ message: "Username is required." });
+  }
+
+  if (values.username && (values.username.length < 8 || values.username.length > 16)) {
+    errors.username.push({ message: "Username must be 8-16 characters." });
+  }
+
+  if (values.username && !/^[a-zA-Z0-9]+$/.test(values.username)) {
+    errors.username.push({ message: "Username must only be alphanumeric characters." });
+  }
+
+  return { errors };
+};
+
+const onSubmit = async (event: FormSubmitEvent) => {
+  if (!event.valid) {
+    event.errors.username?.forEach((e: any) => {
+      toast.add({
+        severity: "error",
+        summary: "Form Validation",
+        detail: e.message,
+        life: 3000,
+      });
+    });
+
+    return;
+  }
+  
+  try {
+    loading.value = true;
+
+    const response = await $fetch<IRes<IUser>>(
+      `/api/users/${auth.user?.id}`,
+      {
+        method: HTTP_METHOD.PATCH,
+        body: { username: event.states.username?.value }
+      },
+    );
+
+    if (response.statusCode === HTTP_STATUS.OK) {
+      await auth.fetchUser();
+      visible.value = false;
+      toast.add({
+        severity: "success",
+        summary: "Success",
+        detail: response.statusMessage,
+        life: 3000,
+      });
+    }
+  } catch (error) {
+    const { statusMessage } = error as IRes || {};
+    toast.add({
+      severity: "error",
+      summary: "Error",
+      detail: statusMessage,
+      life: 3000,
+    });
+  } finally {
+    loading.value = false;
+  }
+};
+</script>
+
+<template>
+<Dialog
+  v-model:visible="visible"
+  header="Change Username"
+  style="width: 25rem;"
+  modal
+>
+  <Form
+    v-slot="$form"
+    :key="formKey"
+    :initialValues="initialValues"
+    :resolver="resolver"
+    @submit="onSubmit"
+    class="user-form"
+  >
+    <span>Your username will be part of all of your shortlinks so make it catchy!</span>
+    <div class="user-form__group">
+      <label for="username">Username</label>
+      <InputText
+        fluid
+        id="username"
+        name="username"
+        :disabled="loading"
+      />
+      <Message
+        v-for="error in $form.username?.errors"
+        severity="error"
+        variant="simple"
+        size="small"
+      >
+        {{ error.message }}
+      </Message>
+    </div>
+
+    <div class="user-form__buttons">
+      <Button
+        severity="secondary"
+        label="Cancel"
+        @click="visible = false"
+        :disabled="loading"
+      />
+      <Button
+        type="submit"
+        severity="contrast"
+        label="Change"
+        :loading="loading"
+      />
+    </div>
+  </Form>
+</Dialog>
+</template>
+
+<style scoped lang="scss">
+.user-form {
+  display: flex;
+  flex-direction: column;
+  gap: 2.50rem;
+
+  &__group {
+    display: flex;
+    flex-direction: column;
+    gap: 0.25rem;
+  }
+
+  &__buttons {
+    display: flex;
+    justify-content: end;
+    gap: 1.25rem;
+  }
+}
+</style>
--- app/pages/dashboard.vue
+++ app/pages/dashboard.vue
@@ -1,15 +1,73 @@
 <script setup lang="ts">
 definePageMeta({ middleware: "auth", layout: "shell" });
 
-const { user } = useAuthStore();
+const auth = useAuthStore();
+const confirm = useConfirm();
+
+const popoverRef = ref();
+const isShowChangeUsername = ref(false);
+const userSettings = ref(
+  [
+    {
+      label: "Account Settings",
+      items: [
+        {
+          label: "Change Username",
+          icon: "pi pi-tag",
+          action: () => isShowChangeUsername.value = true,
+        },
+      ]
+    }
+  ]
+);
+
+const toggleUserSettings = (event: any) => {
+  popoverRef.value.toggle(event)
+};
 </script>
 
 <template>
-<div v-if="user">
-  <p>Welcome, {{ user.username }}.</p>
-</div>
+<Toolbar v-if="auth.user" class="user-info">
+  <template #start>
+    <h4>Welcome, {{ auth.user.username }}.</h4>
+  </template>
+
+  <template #end>
+    <Button
+      severity="contrast"
+      variant="text"
+      icon="pi pi-cog"
+      label="Settings"
+      @click="toggleUserSettings"
+    />
+
+    <Popover ref="popoverRef">
+      <Menu :model="userSettings" class="user-settings">
+        <template #submenuheader="{ item }">
+          <span class="user-settings__title">{{ item.label }}</span>
+        </template>
+        <template #item="{ item }">
+          <div role="button" v-ripple class="p-ripple user-settings__item" @click="item.action">
+            <i :class="item.icon" />
+            <span>{{ item.label }}</span>
+          </div>
+        </template>
+      </Menu>
+    </Popover>
+  </template>
+</Toolbar>
+
+<ChangeUsernameModal v-model:visible="isShowChangeUsername" />
 </template>
 
 <style scoped lang="scss">
-
+.user-settings {
+  &__item {
+    display: flex;
+    gap: 0.75rem;
+    padding: 0.75rem;
+    cursor: pointer;
+    user-select: none;
+  }
+}
 </style>
