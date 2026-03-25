56274b5d43d539f85d97fb314e742823fa20a166
ram-a-dhan <ram.adhan@outlook.com>
2026-03-25 11:23:48 +0700

    feat: cors allowlist

--- server/middleware/cors.ts
+++ server/middleware/cors.ts
@@ -1,11 +1,18 @@
 export default defineEventHandler((event) => {
-  setResponseHeaders(event, {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-    "Access-Control-Allow-Headers": "Content-Type, Authorization",
-    "Access-Control-Allow-Credentials": "true", // Allow cookies and auth headers
-    "Access-Control-Max-Age": "86400", // Cache preflight response for 10 minutes in development, 24 hours in production
-  });
+  const origin = getRequestHeaders(event)?.origin;
+  const baseUrl = useRuntimeConfig()?.public.BASE_URL;
+
+  const allowList = [baseUrl];
+
+  if (origin && allowList.includes(origin)) {
+    setResponseHeaders(event, {
+      "Access-Control-Allow-Origin": origin,
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      "Access-Control-Allow-Credentials": "true", // Allow cookies and auth headers
+      "Access-Control-Max-Age": import.meta.dev ? "600" : "86400", // Cache preflight response for 10 minutes in development, 24 hours in production
+    });
+  }
 
   if (event.method === "OPTIONS") {
     return sendNoContent(event); // Respond to preflight requests with 204 No Content
