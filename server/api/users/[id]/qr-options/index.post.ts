d5e62fc6bb7a1e41ca52d237cd78324e6879b7cd
ram-a-dhan <ram.adhan@outlook.com>
2026-03-28 13:53:30 +0700

    feat: qr options event handlers

---
+++ server/api/users/[id]/qr-options/index.post.ts
@@ -0,0 +1,44 @@
+import { qrOptions as qrOptionSchema } from "#server/database/schema";
+import { IQrOptions } from "#shared/types/data";
+
+export default defineEventHandler(async (event) => {
+  const userId = getRouterParams(event)?.id;
+  const body = await readBody<IQrOptions>(event);
+
+  if (!userId) throw createError({
+    statusCode: HTTP_STATUS.BAD_REQUEST,
+    statusMessage: "User ID required.",
+  });
+
+  const db = useDb();
+
+  const qrOptions = await db
+    .insert(qrOptionSchema)
+    .values({
+      userId,
+      style: body.style,
+      color1: body.color1,
+      color2: body.color2,
+      gradientType: body.gradientType,
+      gradientAngle: body.gradientAngle,
+      imageUrl: body.imageUrl,
+    })
+    .onConflictDoUpdate({
+      target: qrOptionSchema.userId,
+      set: {
+        style: body.style,
+        color1: body.color1,
+        color2: body.color2,
+        gradientType: body.gradientType,
+        gradientAngle: body.gradientAngle,
+        imageUrl: body.imageUrl,
+      },
+    })
+    .returning();
+
+  return {
+    statusCode: HTTP_STATUS.CREATED,
+    statusMessage: "QR Options saved.",
+    data: qrOptions,
+  };
+});
